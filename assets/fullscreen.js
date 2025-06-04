e49f11df215e688c0719f0eb6f0db2bfc807c4b2
Your Name <you@example.com>
2025-06-03 17:09:20 -0700

    Primer commit

--- assets/fullscreen.js
+++ assets/fullscreen.js
@@ -3,13 +3,23 @@ window.dash_clientside = Object.assign({}, window.dash_clientside, {
         activarPantallaCompleta: function(n_clicks) {
             const elem = document.getElementById("mapa-container");
             if (!elem) return window.dash_clientside.no_update;
-            if (elem.requestFullscreen) {
-                elem.requestFullscreen();
-            } else if (elem.webkitRequestFullscreen) {
-                elem.webkitRequestFullscreen();
-            } else if (elem.msRequestFullscreen) {
-                elem.msRequestFullscreen();
+
+            function requestFull(el) {
+                if (el.requestFullscreen) {
+                    el.requestFullscreen();
+                } else if (el.webkitRequestFullscreen) {
+                    el.webkitRequestFullscreen();
+                } else if (el.msRequestFullscreen) {
+                    el.msRequestFullscreen();
+                }
             }
+
+            requestFull(elem);
+
+            setTimeout(() => {
+                window.dispatchEvent(new Event('resize'));
+            }, 1000);
+
             return 0;
         }
     }
